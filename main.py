f9f66e8f40f47792eb8418422d0aba6a337f83d1
az01110392929-maker <az01110392929@gmail.com>
2026-08-28 17:36:42 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.constants import ParseMode
 from telegram.ext import ApplicationBuilder, CommandHandler, CallbackQueryHandler, MessageHandler, filters, ContextTypes
 
+# بياناتك الخاصة بشركة بورسعيد
 BOT_TOKEN = "8925183383:AAHJs1AW5QcKok-DlYUMjZds1239dW1HPNQ"
 BOT_USERNAME = "PortSaid_Store_bot"
 WHATSAPP_NUMBER = "201000744741"
