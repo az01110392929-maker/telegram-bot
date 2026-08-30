2a0a9036eb5c1ee1dc6acba1cc2d74df6d1ae293
az01110392929-maker <az01110392929@gmail.com>
2026-08-30 21:15:59 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -516,3 +516,4 @@ def main():
     app.add_handler(CallbackQueryHandler(delete_single_item, pattern="^del_\\d+$"))
     app.add_handler(CallbackQueryHandler(handle_qty, pattern="^add_"))
     app.add_handler(CallbackQueryHandler(custom_qty, pattern="^cust
+ 
