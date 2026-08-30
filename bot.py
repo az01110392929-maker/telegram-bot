e0356baec667f508c30ae4904044d46f6dac3264
az01110392929-maker <az01110392929@gmail.com>
2026-08-30 15:28:02 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,7 +8,7 @@ from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.constants import ParseMode
 from telegram.ext import Application, CommandHandler, CallbackQueryHandler, MessageHandler, filters, ContextTypes
 
-BOT_TOKEN = "8925183383:AAFRVd4AdWNtE_vW15Po8qjqBbjx5r93Dn4"
+BOT_TOKEN = "8925183383:AAGjmv-1lQHV4dSgaSw2R4wU1m6qqI88Iak"
 BOT_USERNAME = "PortSaid_Store_bot"
 WHATSAPP_NUMBER = "201000744741"
 DEFAULT_CHANNEL_LINK = "https://t.me/Clothing010"
