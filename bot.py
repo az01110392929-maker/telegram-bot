c3fb4367021fa6a8f4794fa6aa864d996651c82d
az01110392929-maker <az01110392929@gmail.com>
2026-08-31 04:19:10 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -518,4 +518,3 @@ def main():
     app.add_handler(CallbackQueryHandler(view_cart, pattern="^view_cart$"))
     app.add_handler(CallbackQueryHandler(clear_cart, pattern="^clear_cart$"))
     app.add_handler(CallbackQueryHandler(send_wa, pattern="^send_wa
- 
