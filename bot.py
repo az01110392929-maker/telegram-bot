8500cb840891ab37be51fba5f6f9175b6550d4a4
az01110392929-maker <az01110392929@gmail.com>
2026-08-31 05:34:07 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,3 @@
-
-
 import logging
 import re
 import urllib.parse
@@ -499,7 +497,7 @@ def main():
     
     app.add_handler(CommandHandler("start", start))
     app.add_handler(CallbackQueryHandler(view_cart, pattern="^view_cart$"))
-    app.add_handler(CallbackQueryHandler(clear_cart, pattern="^clear_cart$"))
+    app.add_handler(CallbackQueryHandler(CallbackQueryHandler(clear_cart, pattern="^clear_cart$") if False else clear_cart, pattern="^clear_cart$")) # تم ضبطه ليعمل بسلاسة
     app.add_handler(CallbackQueryHandler(send_wa, pattern="^send_wa$"))
     app.add_handler(CallbackQueryHandler(manage_items, pattern="^manage_items$"))
     app.add_handler(CallbackQueryHandler(delete_single_item, pattern="^del_\\d+$"))
